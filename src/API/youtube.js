a128e70d1d98642da685f9affb6ce1afb70357ce
Lena Kujath <l.kujath@gmx.de>
2020-03-17 17:36:21 +0100

    Created a basic search bar that will return 10 videos after ententering the search

---
+++ src/API/youtube.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const KEY = '';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3/',
+    params: {
+        part: 'snippet',
+        maxResults: 10,
+        key: KEY
+    }
+})
